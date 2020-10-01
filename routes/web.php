337d196995ffdb4fc13780facc2b898952ac273d
hponemahn <hponemahn@gmail.com>
2020-10-01 10:47:01 +0630

    item cat profit

--- routes/web.php
+++ routes/web.php
@@ -20,22 +20,23 @@ use App\Product;
 
 Route::get('/test', function () {
 
-  $res = DB::table('orders')
-  ->join("order_details", "orders.id", "=", "order_details.order_id")
-  ->join("products", "order_details.product_id", "=", "products.id")
-  ->select('products.name as name', DB::raw('SUM(order_details.qty) as qty'), DB::raw('SUM((order_details.qty * order_details.price) - (order_details.qty * products.buy_price)) as total'), DB::raw("DATE_FORMAT(orders.order_date, '%Y') year"), DB::raw("DATE_FORMAT(orders.order_date, '%m') month"), DB::raw('MONTH(orders.order_date) months'))
-  // DB::raw('SUM((buy_price - discount_price) - sell_price) as total')
-  // ->whereBetween('order_date', [$dateS, $dateE])
-  ->groupby('name', 'month', 'year', 'months')
-  // ->groupby('')
-  ->orderBy('year', 'DESC')
-  ->orderBy('months', 'DESC')
-  // ->limit(5)
-  ->get()
-  // ->reverse()
-  ->all();   
-  
-      return $res;    
+    $res = DB::table('orders')
+    ->join("order_details", "orders.id", "=", "order_details.order_id")
+    ->join("products", "order_details.product_id", "=", "products.id")
+    ->join("categories", "products.category_id", "=", "categories.id")
+    ->select('categories.name as name', DB::raw('SUM(order_details.qty) as qty'), DB::raw('SUM((order_details.qty * order_details.price) - (order_details.qty * products.buy_price)) as total'), DB::raw("DATE_FORMAT(orders.order_date, '%Y') year"), DB::raw("DATE_FORMAT(orders.order_date, '%m') month"), DB::raw('MONTH(orders.order_date) months'))
+    // DB::raw('SUM((buy_price - discount_price) - sell_price) as total')
+    // ->whereBetween('order_date', [$dateS, $dateE])
+    ->groupby('name', 'month', 'year', 'months')
+    // ->groupby('')
+    ->orderBy('year', 'DESC')
+    ->orderBy('months', 'DESC')
+    // ->limit(5)
+    ->get()
+    // ->reverse()
+    ->all();   
+    
+    return $res;
 
 });
 
