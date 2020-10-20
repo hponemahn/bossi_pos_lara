650d6335002166e3e7bd733c4dfadd7ec5555f36
hponemahn <hponemahn@gmail.com>
2020-10-20 18:06:49 +0630

    product paginate

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        for ($i = 0; $i < 500; $i++){
+            DB::table('products')->insert([ //,
+                'name' => $faker->word,
+                'category_id' => 2,
+                'stock' => 50,
+                'buy_price' => 2000,
+                'sell_price' => 6000,
+                'discount_price' => 5000,
+                'sku' => $faker->word,
+                
+            ]);
+        }
+    }
+}
