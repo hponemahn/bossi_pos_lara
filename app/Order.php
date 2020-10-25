0c5011f1a8cb4c035280139e85d644dcfc6ccd00
hponemahn <hponemahn@gmail.com>
2020-10-25 12:49:58 +0630

    item cat profit report paginate

--- app/Order.php
+++ app/Order.php
@@ -276,4 +276,111 @@ class Order extends Model
 
       return $res; 
     }
+
+    public function getItemCatProfit($_, array $args)
+    {
+
+      $res;
+      
+      if($args['startDate'] != "0" && $args['endDate'] != "0" && $args['filter'] == "m"){
+        
+        $res = DB::table('orders')
+        ->join("order_details", "orders.id", "=", "order_details.order_id")
+        ->join("products", "order_details.product_id", "=", "products.id")
+        ->join("categories", "products.category_id", "=", "categories.id")
+        ->select('categories.name as name', DB::raw('SUM(order_details.qty) as qty'), DB::raw('SUM((order_details.qty * order_details.price) - (order_details.qty * products.buy_price)) as total'), DB::raw("DATE_FORMAT(orders.order_date, '%Y') year"), DB::raw("DATE_FORMAT(orders.order_date, '%m') month"), DB::raw('MONTH(orders.order_date) months'))
+        ->whereBetween('orders.order_date', [$args['startDate'], $args['endDate']])
+        ->groupby('name', 'month', 'year', 'months')
+        ->orderBy('year', 'DESC')
+        ->orderBy('months', 'DESC');
+        // ->limit(5)
+        
+        // ->reverse()
+        
+
+      } elseif ($args['startDate'] != "0" && $args['endDate'] != "0" && $args['filter'] == "y") {
+        
+        $res = DB::table('orders')
+        ->join("order_details", "orders.id", "=", "order_details.order_id")
+        ->join("products", "order_details.product_id", "=", "products.id")
+        ->join("categories", "products.category_id", "=", "categories.id")
+        ->select('categories.name as name', DB::raw('SUM(order_details.qty) as qty'), DB::raw('SUM((order_details.qty * order_details.price) - (order_details.qty * products.buy_price)) as total'), DB::raw("DATE_FORMAT(orders.order_date, '%Y') year"))
+        ->whereBetween('orders.order_date', [$args['startDate'], $args['endDate']])
+        ->groupby('name', 'year')
+        ->orderBy('year', 'DESC');
+        // ->limit(5)
+        
+        // ->reverse()
+        
+      } elseif ($args['startDate'] != "0" && $args['endDate'] != "0" && $args['filter'] == "d") {
+
+        $res = DB::table('orders')
+        ->join("order_details", "orders.id", "=", "order_details.order_id")
+        ->join("products", "order_details.product_id", "=", "products.id")
+        ->join("categories", "products.category_id", "=", "categories.id")
+        ->select('categories.name as name', DB::raw('SUM(order_details.qty) as qty'), DB::raw('SUM((order_details.qty * order_details.price) - (order_details.qty * products.buy_price)) as total'), DB::raw("DATE_FORMAT(order_date, '%d') day"), DB::raw("DATE_FORMAT(orders.order_date, '%Y') year"), DB::raw("DATE_FORMAT(orders.order_date, '%m') month"), DB::raw('MONTH(orders.order_date) months'))
+        ->whereBetween('orders.order_date', [$args['startDate'], $args['endDate']])
+        ->groupby('name', 'day', 'month', 'year', 'months')
+        ->orderBy('year', 'DESC')
+        ->orderBy('months', 'DESC')
+        ->orderBy('day', 'DESC');
+        // ->limit(5)
+        
+        // ->reverse()
+        
+        
+      } elseif ($args['filter'] == "y") {
+
+        $res = DB::table('orders')
+        ->join("order_details", "orders.id", "=", "order_details.order_id")
+        ->join("products", "order_details.product_id", "=", "products.id")
+        ->join("categories", "products.category_id", "=", "categories.id")
+        ->select('categories.name as name', DB::raw('SUM(order_details.qty) as qty'), DB::raw('SUM((order_details.qty * order_details.price) - (order_details.qty * products.buy_price)) as total'), DB::raw("DATE_FORMAT(orders.order_date, '%Y') year"))
+        // DB::raw('SUM((buy_price - discount_price) - sell_price) as total')
+        // ->whereBetween('order_date', [$dateS, $dateE])
+        ->groupby('name', 'year')
+        ->orderBy('year', 'DESC');
+        // ->limit(5)
+        
+        // ->reverse()
+        
+
+      } elseif ($args['filter'] == "d") {
+
+        $res = DB::table('orders')
+        ->join("order_details", "orders.id", "=", "order_details.order_id")
+        ->join("products", "order_details.product_id", "=", "products.id")
+        ->join("categories", "products.category_id", "=", "categories.id")
+        ->select('categories.name as name', DB::raw('SUM(order_details.qty) as qty'), DB::raw('SUM((order_details.qty * order_details.price) - (order_details.qty * products.buy_price)) as total'), DB::raw("DATE_FORMAT(order_date, '%d') day"), DB::raw("DATE_FORMAT(orders.order_date, '%Y') year"), DB::raw("DATE_FORMAT(orders.order_date, '%m') month"), DB::raw('MONTH(orders.order_date) months'))
+        // DB::raw('SUM((buy_price - discount_price) - sell_price) as total')
+        // ->whereBetween('order_date', [$dateS, $dateE])
+        ->groupby('name', 'day', 'month', 'year', 'months')
+        ->orderBy('year', 'DESC')
+        ->orderBy('months', 'DESC')
+        ->orderBy('day', 'DESC');
+        // ->limit(5)
+        
+        // ->reverse()
+        
+
+      } else {
+        $res = DB::table('orders')
+              ->join("order_details", "orders.id", "=", "order_details.order_id")
+              ->join("products", "order_details.product_id", "=", "products.id")
+              ->join("categories", "products.category_id", "=", "categories.id")
+              ->select('categories.name as name', DB::raw('SUM(order_details.qty) as qty'), DB::raw('SUM((order_details.qty * order_details.price) - (order_details.qty * products.buy_price)) as total'), DB::raw("DATE_FORMAT(orders.order_date, '%Y') year"), DB::raw("DATE_FORMAT(orders.order_date, '%m') month"), DB::raw('MONTH(orders.order_date) months'))
+              // DB::raw('SUM((buy_price - discount_price) - sell_price) as total')
+              // ->whereBetween('order_date', [$dateS, $dateE])
+              ->groupby('name', 'month', 'year', 'months')
+              // ->groupby('')
+              ->orderBy('year', 'DESC')
+              ->orderBy('months', 'DESC');
+              // ->limit(5)
+              
+              // ->reverse()
+              
+      }
+
+      return $res; 
+    }
 }
