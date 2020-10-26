b69b4cc6122b1e36c9209d54aeef69e2e519572a
hponemahn <hponemahn@gmail.com>
2020-10-26 16:30:22 +0630

    product report paginate

--- app/Product.php
+++ app/Product.php
@@ -47,29 +47,29 @@ class Product extends Model
             $res = DB::table('products')->select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%Y') year"), DB::raw("DATE_FORMAT(created_at, '%m') month"), DB::raw('MONTH(created_at) months'))
                 ->whereBetween('created_at', [$args['startDate'], $args['endDate']])
                 ->groupby('month', 'year', 'months')
-            ->orderBy('year', 'DESC')
-            ->orderBy('months', 'DESC');
+                ->orderBy('year', 'DESC')
+                ->orderBy('months', 'DESC');
 
         } elseif ($args['startDate'] != "0" && $args['endDate'] != "0" && $args['filter'] == "y") {
 
             $res = DB::table('products')->select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%Y') year"))
                 ->whereBetween('created_at', [$args['startDate'], $args['endDate']])
                 ->groupby('year')
-            ->orderBy('year', 'DESC');
+                ->orderBy('year', 'DESC');
 
         } elseif ($args['startDate'] != "0" && $args['endDate'] != "0" && $args['filter'] == "d") {
 
             $res = DB::table('products')->select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%d') day"), DB::raw("DATE_FORMAT(created_at, '%Y') year"), DB::raw("DATE_FORMAT(created_at, '%m') month"), DB::raw('MONTH(created_at) months'))
                 ->whereBetween('created_at', [$args['startDate'], $args['endDate']])
                 ->groupby('day', 'month', 'year', 'months')
-            ->orderBy('year', 'DESC')
-            ->orderBy('months', 'DESC')
-            ->orderBy('day', 'DESC');
+                ->orderBy('year', 'DESC')
+                ->orderBy('months', 'DESC')
+                ->orderBy('day', 'DESC');
 
         } elseif ($args['filter'] == "y") {
             $res = DB::table('products')->select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%Y') year"))
                 ->groupby('year')
-            ->orderBy('year', 'DESC');
+                ->orderBy('year', 'DESC');
 
         } elseif ($args['filter'] == "d") {
             $res = DB::table('products')->select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%d') day"), DB::raw("DATE_FORMAT(created_at, '%Y') year"), DB::raw("DATE_FORMAT(created_at, '%m') month"), DB::raw('MONTH(created_at) months'))
@@ -92,52 +92,136 @@ class Product extends Model
 
     public function getBuy($_, array $args)
     {
-      $res;
-      
-      if($args['startDate'] != "0" && $args['endDate'] != "0" && $args['filter'] == "m"){
+        $res;
 
-        $res = Product::select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%Y') year"), DB::raw("DATE_FORMAT(created_at, '%m') month"), DB::raw('MONTH(created_at) months'))
-        ->whereBetween('created_at', [$args['startDate'], $args['endDate']])
-        ->groupby('month', 'year', 'months')
-        ->orderBy('year', 'DESC')
-        ->orderBy('months', 'DESC');
-        // ->limit(5)
-        // ->reverse()
+        if ($args['startDate'] != "0" && $args['endDate'] != "0" && $args['filter'] == "m") {
 
-      } elseif ($args['startDate'] != "0" && $args['endDate'] != "0" && $args['filter'] == "y") {
+            $res = Product::select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%Y') year"), DB::raw("DATE_FORMAT(created_at, '%m') month"), DB::raw('MONTH(created_at) months'))
+                ->whereBetween('created_at', [$args['startDate'], $args['endDate']])
+                ->groupby('month', 'year', 'months')
+                ->orderBy('year', 'DESC')
+                ->orderBy('months', 'DESC');
+            // ->limit(5)
+            // ->reverse()
 
-        $res = Product::select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%Y') year"))
-        ->whereBetween('created_at', [$args['startDate'], $args['endDate']])
-        ->groupby('year')
-        ->orderBy('year', 'DESC');
-      } elseif ($args['startDate'] != "0" && $args['endDate'] != "0" && $args['filter'] == "d") {
-        
-        $res = Product::select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%d') day"), DB::raw("DATE_FORMAT(created_at, '%Y') year"), DB::raw("DATE_FORMAT(created_at, '%m') month"), DB::raw('MONTH(created_at) months'))
-        ->whereBetween('created_at', [$args['startDate'], $args['endDate']])
-        ->groupby('day', 'month', 'year', 'months')
-        ->orderBy('year', 'DESC')
-        ->orderBy('months', 'DESC')
-        ->orderBy('day', 'DESC');
-      } elseif ($args['filter'] == "y") {
-        $res = Product::select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%Y') year"))
-        ->groupby('year')
-        ->orderBy('year', 'DESC');
-      } elseif ($args['filter'] == "d") {
-        $res = Product::select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%d') day"), DB::raw("DATE_FORMAT(created_at, '%Y') year"), DB::raw("DATE_FORMAT(created_at, '%m') month"), DB::raw('MONTH(created_at) months'))
-        ->groupby('day', 'month', 'year', 'months')
-        ->orderBy('year', 'DESC')
-        ->orderBy('months', 'DESC')
-        ->orderBy('day', 'DESC');
-      } else {
-        $res = Product::select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%Y') year"), DB::raw("DATE_FORMAT(created_at, '%m') month"), DB::raw('MONTH(created_at) months'))
-        // ->whereBetween('order_date', [$dateS, $dateE])
-        ->groupby('month', 'year', 'months')
-        ->orderBy('year', 'DESC')
-        ->orderBy('months', 'DESC');
-        // ->limit(5)
-        // ->reverse()
-      }
+        } elseif ($args['startDate'] != "0" && $args['endDate'] != "0" && $args['filter'] == "y") {
 
-      return $res;
+            $res = Product::select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%Y') year"))
+                ->whereBetween('created_at', [$args['startDate'], $args['endDate']])
+                ->groupby('year')
+                ->orderBy('year', 'DESC');
+        } elseif ($args['startDate'] != "0" && $args['endDate'] != "0" && $args['filter'] == "d") {
+
+            $res = Product::select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%d') day"), DB::raw("DATE_FORMAT(created_at, '%Y') year"), DB::raw("DATE_FORMAT(created_at, '%m') month"), DB::raw('MONTH(created_at) months'))
+                ->whereBetween('created_at', [$args['startDate'], $args['endDate']])
+                ->groupby('day', 'month', 'year', 'months')
+                ->orderBy('year', 'DESC')
+                ->orderBy('months', 'DESC')
+                ->orderBy('day', 'DESC');
+        } elseif ($args['filter'] == "y") {
+            $res = Product::select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%Y') year"))
+                ->groupby('year')
+                ->orderBy('year', 'DESC');
+        } elseif ($args['filter'] == "d") {
+            $res = Product::select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%d') day"), DB::raw("DATE_FORMAT(created_at, '%Y') year"), DB::raw("DATE_FORMAT(created_at, '%m') month"), DB::raw('MONTH(created_at) months'))
+                ->groupby('day', 'month', 'year', 'months')
+                ->orderBy('year', 'DESC')
+                ->orderBy('months', 'DESC')
+                ->orderBy('day', 'DESC');
+        } else {
+            $res = Product::select(DB::raw('SUM(buy_price) as total'), DB::raw("DATE_FORMAT(created_at, '%Y') year"), DB::raw("DATE_FORMAT(created_at, '%m') month"), DB::raw('MONTH(created_at) months'))
+            // ->whereBetween('order_date', [$dateS, $dateE])
+                ->groupby('month', 'year', 'months')
+                ->orderBy('year', 'DESC')
+                ->orderBy('months', 'DESC');
+            // ->limit(5)
+            // ->reverse()
+        }
+
+        return $res;
+    }
+
+    public function getMostBuyingItem($_, array $args)
+    {
+        return DB::table('products')
+            ->select('name', 'stock as qty', DB::raw('SUM(buy_price * stock) as total'))
+            ->groupby('name', 'qty')
+            ->orderBy('total', 'DESC');
+    }
+
+    public function getMostBuyingItemCat($_, array $args)
+    {
+        return DB::table('products')
+            ->join("categories", "products.category_id", "=", "categories.id")
+            ->select('categories.name as catName', DB::raw('SUM(products.buy_price * products.stock) as total'), DB::raw('ANY_VALUE(SUM(products.stock)) as qty'))
+            ->groupby('catName')
+            ->orderBy('total', 'DESC');
+    }
+
+    public function getLeastBuyingItem($_, array $args)
+    {
+        return DB::table('products')
+            ->select('name', 'stock as qty', DB::raw('SUM(buy_price * stock) as total'))
+            ->groupby('name', 'qty')
+            ->orderBy('total', 'ASC');
+    }
+
+    public function getLeastBuyingItemCat($_, array $args)
+    {
+        return DB::table('products')
+            ->join("categories", "products.category_id", "=", "categories.id")
+            ->select('categories.name as catName', DB::raw('SUM(products.buy_price * products.stock) as total'), DB::raw('ANY_VALUE(SUM(products.stock)) as qty'))
+            ->groupby('catName')
+            ->orderBy('total', 'ASC');
+    }
+
+    public function getTotalItem($_, array $args)
+    {
+        return DB::table('products')
+            ->select('name', DB::raw('SUM(stock) as qty'))
+            ->groupby('name');
+    }
+
+    public function getMostItem($_, array $args)
+    {
+        return DB::table('products')
+            ->select('name', DB::raw('SUM(stock) as qty'))
+            ->groupby('name')
+            ->orderBy('qty', 'DESC');
+    }
+
+    public function getLeastItem($_, array $args)
+    {
+        return DB::table('products')
+            ->select('name', DB::raw('SUM(stock) as qty'))
+            ->groupby('name')
+            ->orderBy('qty', 'ASC');
+    }
+
+    public function getDamagedItem($_, array $args)
+    {
+        return DB::table('products')
+            ->select('name', DB::raw('SUM(stock) as qty'))
+            ->where('is_damaged', 1)
+            ->groupby('name')
+            ->orderBy('qty', 'DESC');
+    }
+
+    public function getLostItem($_, array $args)
+    {
+        return DB::table('products')
+            ->select('name', DB::raw('SUM(stock) as qty'))
+            ->where('is_lost', 1)
+            ->groupby('name')
+            ->orderBy('qty', 'DESC');
+    }
+
+    public function getExpiredItem($_, array $args)
+    {
+        return DB::table('products')
+            ->select('name', DB::raw('SUM(stock) as qty'))
+            ->where('is_expired', 1)
+            ->groupby('name')
+            ->orderBy('qty', 'DESC');
     }
 }
